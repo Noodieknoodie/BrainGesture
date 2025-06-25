a8c6acebe88d6a6f7012c9d34bc4d34262f51c87
Noodieknoodie <127708260+Noodieknoodie@users.noreply.github.com>
2025-06-24 17:17:56 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/BrainGesture/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
